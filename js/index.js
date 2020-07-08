65cdca49e84f82f430b15418d3edc12266faf061
Gabriel Albino <enggabrielalbino@gmail.com>
2020-07-07 23:19:49 -0300

    Build para homol

--- js/index.js
+++ js/index.js
@@ -295,6 +295,7 @@ $(document).ready(function () {
     const mail = $("#formMail").val();
     if (verifyFields(name, phone, mail)) {
       var mailData = {
+        dest: "mariannaterto@gmail.com",
         nome: name,
         codigo: number,
         requerido: "",
